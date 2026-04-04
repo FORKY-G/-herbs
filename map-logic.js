a3e9d895407fe6fbb5c33e9f3c856f7b1d3abad9
FORKY-G <forky.yun@gmail.com>
2026-04-04 21:56:58 +0900

    Update map-logic.js

--- map-logic.js
+++ map-logic.js
@@ -1,11 +1,11 @@
-// 1. Leaflet 지도 설정 (평면 좌표계인 CRS.Simple 사용)
+// 1. Leaflet 지도 설정
 const map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: -3, // 축소 한도 (필요에 따라 숫자를 조절하세요)
-    maxZoom: 2   // 확대 한도
+    // minZoom: -3 부분은 아래에서 자동으로 설정할 것이므로 지웠습니다.
+    maxZoom: 3   // 확대 한도 (더 많이 확대하고 싶다면 숫자를 3, 4 등으로 늘리세요)
 });
 
-// 2. 픽셀크기.
+// 2. 픽셀크기
 const imgWidth = 4560;  // 가로
 const imgHeight = 4560; // 세로
 
@@ -15,5 +15,12 @@ const imageBounds = [[0, 0], [imgHeight, imgWidth]];
 // 4. hanwol-map.jpg 이미지 띄우기
 L.imageOverlay('hanwol-map.jpg', imageBounds).addTo(map);
 
-// 5. 처음 화면을 열었을 때 지도가 화면에 꽉 차게(가운데로) 보이도록 설정
+// 5. 처음 화면을 열었을 때 지도가 화면에 꽉 차게 보이도록 설정
 map.fitBounds(imageBounds);
+
+// 6. [추가된 부분] 현재 화면에 꽉 찬 상태의 줌 레벨을 최소 줌(minZoom)으로 고정!
+// 이렇게 하면 지금보다 더 축소할 수 없습니다.
+map.setMinZoom(map.getZoom());
+
+// 7. [추가된 부분] 지도를 드래그할 때 이미지가 화면 밖(회색/검은 여백)으로 도망가지 않게 영역 제한
+map.setMaxBounds(imageBounds);
