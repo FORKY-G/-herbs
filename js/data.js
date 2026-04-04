bf36f95486e6e175cfb5c3d30c141dac64d1e4d3
FORKY-G <forky.yun@gmail.com>
2026-04-05 00:00:23 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,18 @@
-const spawnData = {
-    name: "스폰",
-    mcX: -969,
-    mcZ: -965
-};
+//1. 스폰
+const spawnData = { name: "스폰 지점", mcX: -969, mcZ: -965 };
+
+//2. 십이지신
+const animals = [
+    { order: 1, name: "쥐", mcX: -2900, mcY: 127, mcZ: -4600 },
+    { order: 2, name: "소", mcX: -616, mcY: 116, mcZ: 3889 },
+    { order: 3, name: "호랑이", mcX: -5500, mcY: 152, mcZ: -1600 },
+    { order: 4, name: "토끼", mcX: 4590, mcY: 122, mcZ: -3397 },
+    { order: 5, name: "용", mcX: -891, mcY: 106, mcZ: -536 },
+    { order: 6, name: "뱀", mcX: 2685, mcY: 145, mcZ: -4051 },
+    { order: 7, name: "말", mcX: -2365, mcY: 282, mcZ: 3968 },
+    { order: 8, name: "양", mcX: 5093, mcY: 135, mcZ: 2092 },
+    { order: 9, name: "원숭이", mcX: 3280, mcY: 162, mcZ: 2952 },
+    { order: 10, name: "닭", mcX: 5075, mcY: 106, mcZ: -344 },
+    { order: 11, name: "개", mcX: -4455, mcY: 171, mcZ: -2376 },
+    { order: 12, name: "돼지", mcX: -3657, mcY: 212, mcZ: 1112 }
+];
